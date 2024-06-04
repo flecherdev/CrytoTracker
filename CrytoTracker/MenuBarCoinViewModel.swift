92a6bb4e4a4bd2020061ee909ac3c69733882d15
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2024-06-04 00:21:57 -0300

    added model to menu bar and pop over

---
+++ CrytoTracker/MenuBarCoinViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MenuBarCoinViewModel.swift
+//  CrytoTracker
+//
+//  Created by Ezequiel Freire on 03/06/2024.
+//
+
+import Foundation
---
+++ CrytoTracker/PopoverCoinViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PopoverCoinViewModel.swift
+//  CrytoTracker
+//
+//  Created by Ezequiel Freire on 03/06/2024.
+//
+
+import Foundation
